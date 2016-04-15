93b870e2f653ddba2ed20fc122e354292ac38278
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-04-15 14:55:26 +0200

    added config for heroku

--- config.bat
+++ config.bat
@@ -1,1 +1,5 @@
-SET MONGOLAB_URI=mongodb://iNote:kalleanka123@ds025180.mlab.com:25180/inotedb
+set username=%1
+@echo off
+set arg2=%2
+
+SET MONGOLAB_URI=mongodb://%username%:%arg2%@ds025180.mlab.com:25180/inotedb
